837f5da08cc3848e70667077f04bb0dab4c8508d
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-09 13:00:23 +0000

    shows info for each house

--- public/app.js
+++ public/app.js
@@ -12,6 +12,13 @@ var requestCompleted = function(){
     var houses = JSON.parse(jsonString);
 
     populateHousesDropDown(houses)
+
+    var houseSelect = document.getElementById('house-select')
+
+    houseSelect.addEventListener('change', function(){
+        var index = houseSelect.selectedIndex;
+        createHouse(index, houses);
+    })
 }
 
 var populateHousesDropDown = function(houses){
@@ -24,41 +31,37 @@ var populateHousesDropDown = function(houses){
     })
 
 }
-// var houseRequest = function(){
-//
-//     var jsonString = this.responseText;
-//     var houses = JSON.parse(jsonString);
-//
-//     createHouse(houses);
-// }
 
-// var createHouse = function(houses){
-//     var ul = document.getElementById("names");
-//
-//     var houseli = document.createElement('li')
-//     houseli.innerText = house.name
-//
-//     var members = house.swornMembers;
-//     console.log(members);
-//     members.forEach(function(member){
-//         var memberli = document.createElement('li')
-//         memberli.innerText = member.name;
-//         houseli.appendChild(memberli);
-//     })
-//
-//     ul.appendChild(houseli);
-// }
+var createHouse = function(index, houses){
+
+    var ul = document.getElementById("house-info");
+    ul.innerHTML = "";
+
+    var name = document.createElement('li');
+    var region = document.createElement('li');
+    var coatOfArms = document.createElement('li');
+    var words = document.createElement('li');
+    var currentLord = document.createElement('li');
+
+    name.innerText = houses[index].name;
+    region.innerText = houses[index].region;
+    coatOfArms.innerText = houses[index].coatOfArms;
+    words.innerText = houses[index].words;
+    currentLord.innerText = houses[index].currentLord;
+
+    ul.appendChild(name);
+    ul.appendChild(region);
+    ul.appendChild(coatOfArms);
+    ul.appendChild(words);
+    ul.appendChild(currentLord);
+
+}
 
 var app = function(){
     var url = "https://anapioficeandfire.com/api/houses?page=1&pageSize=50"
     makeRequest(url, requestCompleted)
 
-    // var houseSelect = document.getElementById('#house-select')
-    //
-    // houseSelect.addEventListener('change', function(){
-    //     var index = houseSelect.selectedIndex;
-    //     makeRequest(url + index, houseRequest);
-    // })
+
 }
 
 window.addEventListener('load', app);
