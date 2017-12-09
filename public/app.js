770c1ae6f935e0de1c9ad63131695fb7b7e56c61
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-09 11:39:02 +0000

    pulls from api

--- public/app.js
+++ public/app.js
@@ -1,5 +1,22 @@
+var makeRequest = function(url, callback){
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('load', callback);
+    request.send();
+}
+
+var requestComplete = function(){
+    if(this.status != 200) return;
+
+    var jsonString = this.responseText;
+    var characters = JSON.parse(jsonString);
+
+}
+
 var app = function(){
-    console.log("Reading");
+    url = "https://anapioficeandfire.com/api/characters"
+    makeRequest(url, requestComplete);
+
 }
 
 window.addEventListener('load', app);
